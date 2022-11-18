411ea2f6d236ecb4171abbd980b0ec5605639958
code-tamer3000 <parfenov-art@mail.ru>
2022-11-19 02:18:08 +0500

    исправил task_4_3.cpp

--- task_4_3.cpp
+++ task_4_3.cpp
@@ -23,12 +23,14 @@ int64_t Merge(std::vector<int64_t>& array, int left_bound, int median, int right
     int64_t answer = 0;
     while (i < median && j < right_bound) {
         if (array[i] < array[j]) {
-            result[i - left_bound + j - median] = array[i]; i++; 
+            result[i - left_bound + j - median] = array[i]; 
+            i++; 
         } else {
             if (array[j] < array[i]) {  
                 answer += median - i;  
             }
-            result[i - left_bound + j - median] = array[j]; j++;
+            result[i - left_bound + j - median] = array[j]; 
+            j++;
         }
     }
     for (; i < median; ++i) {
@@ -44,28 +46,28 @@ int64_t Merge(std::vector<int64_t>& array, int left_bound, int median, int right
 }
 
 
-void MergeSort(std::vector<int64_t>& array, int left_bound, int right_bound, int64_t& answer) {
+void inversion_counter(std::vector<int64_t>& array, int left_bound, int right_bound, int64_t& answer) {
     if (right_bound - left_bound <= 1) {
         return;
     } 
     int median = (right_bound + left_bound) / 2;
-    MergeSort(array, left_bound, median, answer);
-    MergeSort(array, median, right_bound, answer);
+    inversion_counter(array, left_bound, median, answer);
+    inversion_counter(array, median, right_bound, answer);
     answer += Merge(array, left_bound, median, right_bound);
 }
 
-int64_t MergeSort(std::vector<int64_t>& array) {
+int64_t inversion_counter(std::vector<int64_t>& array) {
     int64_t answer = 0;
-    MergeSort(array, 0, array.size(), answer);
+    inversion_counter(array, 0, array.size(), answer);
     return answer;
 }
 
 int main() {
     std::vector<int64_t> array;
     int64_t number = 0;
-    while (std::cin >> number){
+    while (std::cin >> number) {
         array.push_back(number);
     }
-    std::cout << MergeSort(array) << '\n';
+    std::cout << inversion_counter(array) << '\n';
+    return 0;
 }
-
